72e6622484c482fc6ae61a7389e902b5f130e8f8
tom.yu <tom.yu@helimediagroup.com>
2015-04-08 09:11:55 +0800

    commit

--- push.sh
+++ push.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+while read line; do
+	name=`echo $line|awk -F '=' '{print $1}'`
+	value=`echo $line|awk -F '=' '{print $2}'`
+	case $name in
+		"name")
+			name=$value
+			;;
+		"pwd")
+			pwd=$value
+			;;
+		*)
+			;;
+	esac
+done < config.cfg
+echo $name
+echo $pwd
+
 git add --all
 git commit -m "commit"
 git push auth master
